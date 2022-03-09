540341fc7456ac8b2e10b3aeea34a9b1147d8468
errea <erreakezy@gmail.com>
2022-03-09 17:45:04 +0100

    Add Vue model instance to input field + remove nuxt logo

--- components/SignUpForm.vue
+++ components/SignUpForm.vue
@@ -1,18 +1,36 @@
 <template>
-    <form class="w-full max-w-lg">
+<form 
+class="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mt-12"
+action="javascript:;" @submit.prevent="submitForm"
+>
+  <div class="mb-6">
+    <p class="flex justify-center font-bold text-4xl">
+      Register
+    </p>
+     <span class="mb-6 font-medium">Create your account, it is free and only takes Minutes</span>
+  </div>
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         First Name
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+      <input id="grid-first-name" v-model="user.firstname" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Jane">
       <p class="text-red-500 text-xs italic">Please fill out this field.</p>
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
         Last Name
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+      <input id="grid-last-name" v-model="user.lastname" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Doe">
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
+        Email 
+      </label>
+      <input id="grid-email" v-model="user.email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" required placeholder="erreakezy@gmail.com">
+      <p class="text-gray-600 text-xs italic">Enter your email</p>
     </div>
   </div>
   <div class="flex flex-wrap -mx-3 mb-6">
@@ -20,45 +38,97 @@
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Password
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
+      <input id="grid-password" v-model="user.password" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="password" placeholder="******************">
       <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-2">
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-        City
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+        Confirm Password
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque">
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        State
-      </label>
-      <div class="relative">
-        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-        </div>
-      </div>
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-        Zip
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210">
+      <input id="grid-password" v-model="user.passwordChck" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="password" placeholder="******************">
+      <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
     </div>
   </div>
+  <div class="w-full mb-6">
+      <label class="w-full flex text-gray-500 font-bold">
+      <input v-model="user.terms" class="mr-2 mt-1 leading-tight cursor-pointer" type="checkbox" required>
+      <p class="text-sm">
+        By clicking on the Sign Up button, you agree to our <span class="text-blue-400 hover:text-sky-400 cursor-pointer"> Terms & Conditions of use  </span> and <span class="text-blue-400 hover:text-sky-400 cursor-pointer "> Privacy Policy </span>
+      </p>
+      </label>
+    </div>
+   <div class="flex justify-center">
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Register
+      </button>
+   </div>
+   <p>Email: {{user.email}}</p>
+   <p>Password: {{user.password}}</p>
+   <p>Terms: {{user.terms}}</p>
 </form>
-
 </template>
 
 <script>
 export default {
-  name: 'SiginForm',
+  name: 'SignUpForm',
+    data () {
+        return { 
+            user : {
+                firstname:'',
+                lastname:'',
+                email:'',
+                password:'',
+                passwordChck:'',
+                terms: false, 
+            }
+        }
+    },
+    computed: {
+        isFormValid () {
+            return (
+                this.isValid('firstname') && 
+                this.isValid('lastname') && 
+                this.isValid('email') && 
+                this.isValid('password') && 
+                this.isValid('passwordChck')
+            )
+        }
+    },
+    // mounted () {
+    //     const element = this.$el.querySelector('#passwordcheck')
+    //     element.addEventListener('blur', () => {
+    //         if (!this.isValid('passwordChck')) {
+    //             element.classList.add('invalid')
+    //         } else {
+    //             element.classList.remove('invalid')
+    //         }
+    //     })  
+    // },
+    methods: {
+        isValid(prop) {
+        
+        },
+        resetUser () {
+            this.user.firstname = ''
+            this.user.lastname = ''
+            this.user.email = ''
+            this.user.password = ''
+            this.user.passwordChck = ''
+        },
+        onSubmit () {
+            const user = Object.assign({}, this.user)
+            this.resetUser()
+            this.$emit('signup-form', {type:'signup', data:user})
+        }
+    },
+    template:'#registerTemplate',
 }
 </script>
+
+<style>
+/* .nuxt-logo {
+  height: 180px;
+} */
+</style>
