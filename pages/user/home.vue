bedd54aad152694f72c0ed6e57826b164093c426
errea <erreakezy@gmail.com>
2022-03-11 21:42:54 +0100

    ADD: user pages

---
+++ pages/user/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <HomeNav />
+</template>
+
+<script>
+import HomeNav from '~/components/HomeNav'
+
+export default {
+  name: 'HomePage',
+  components: {
+    HomeNav,
+  },
+}
+</script>
