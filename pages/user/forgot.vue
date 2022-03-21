729947b33d96ebf4cc61be52aeb1f6692fbeb6f5
errea <erreakezy@gmail.com>
2022-03-21 09:53:14 +0100

    ADD: forgot password component to users pages

---
+++ pages/user/forgot.vue
@@ -0,0 +1,14 @@
+<template>
+    <ForgotPassword />
+</template>
+
+<script>
+import ForgotPassword from '~/components/ForgotPassword'
+
+export default {
+  name: 'ForgotPasswordPage',
+  components: {
+    ForgotPassword,
+  },
+}
+</script>
