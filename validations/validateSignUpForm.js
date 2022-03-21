75e9d4fa697e3365d73bb51aee9fb8d6b4469f12
errea <erreakezy@gmail.com>
2022-03-21 10:30:34 +0100

    MODIFY: signUp form validation to check password of active user match

--- validations/validateSignUpForm.js
+++ validations/validateSignUpForm.js
@@ -9,22 +9,16 @@ const validateSignUpForm = (data) => {
   if (localStorage.users) {
     let lsUsers = localStorage.users;
     lsUsers = JSON.parse(lsUsers);
-    // const index1 = lsUsers.findIndex((user) => user.firstName === firstName);
-    // const index = lsUsers.findIndex((user) => user.lastName === lastName);
-    const emailindex = lsUsers.findIndex((user) => user.email === email);
-    // const index3 = lsUsers.findIndex((user) => user.password === password);
-    // if (index > -1) {
-    //   errors.lastName = "Last name already exists!";
-    // }
-    // if (index1 > -1) {
-    //   errors.firstName = "First name already exists!";
-    // }
-    if (emailindex > -1) {
+  
+    const emailIndex = lsUsers.findIndex((user) => user.email === email);
+    const passwordIndex = lsUsers.findIndex((user) => user.password === password);
+   
+    if (emailIndex > -1) {
       errors.email = "email already exists!";
     }
-    // if (index3 > -1) {
-    //   errors.password = "Password already exists!";
-    // }
+    if (passwordIndex > -1) {
+      errors.password = "Password already exists!";
+    }
   }
 
   if (validator.isEmpty(firstName)) {
