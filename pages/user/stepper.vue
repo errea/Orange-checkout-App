32546436c4c0b19e595e499fffaaaca32c7cbdb1
errea <erreakezy@gmail.com>
2022-03-21 17:16:19 +0100

    ADD: new components ShoppingList and stepper to pages

---
+++ pages/user/stepper.vue
@@ -0,0 +1,14 @@
+<template>
+    <Stepper />
+</template>
+
+<script>
+import Stepper from '~/components/Stepper'
+
+export default {
+  name: 'StepperFormPage',
+  components: {
+    Stepper,
+  },
+}
+</script>
