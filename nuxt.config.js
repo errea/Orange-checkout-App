1d3c0d1f3949e0341e3af0e07799ea0ae3c87255
errea <erreakezy@gmail.com>
2022-03-21 14:14:28 +0100

    MODIFY: modify nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,6 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   generate: {
-    dir: 'orange-app',
     routes: [
       '/'
     ]
--- orange-app/_nuxt/static/1647866068/user/reg/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/user/reg", {data:[{}],fetch:{},mutations:void 0});
